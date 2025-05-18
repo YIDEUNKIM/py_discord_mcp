1a4f9882e832d591b28970387fc478c49c5014fe
Yideun <rladlems1031@gmail.com>
2025-05-19 07:41:11 +0900

    Feat essential Construct

--- src/mcp_server.py
+++ src/mcp_server.py
@@ -17,22 +17,45 @@ from mcp.server.stdio import stdio_server
 """
 Initial Constructor
 """
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("discord_mcp_server")
 
+#Discord bot setup
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+if not DISCORD_TOKEN:
+    raise ValueError("DISCORD_TOKEN environment variable is required")
 
+# Initialize Discord bot with necessary intents
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(commands_prefix="!", intents=intents)
+
+app = Server("discord_server")
+discord_client = None
 ##################################################
 
-
+@bot.event
 async def ready():
-    pass
+    global dicord_client
+    discord_client = bot
+    logger.info(f"Log as {bot.user.name}")
 
 def require_discord_client():
-    pass
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        if not discord_client:
+            raise RuntimeError("client not ready")
+        return await func(*args, **kwargs)
+    return wrapper
 
 async def list_tools() -> List[Tool]:
     pass
 
+
 async def call_tools(name: str, arg: Any) -> List[TextContent]:
     pass
+
 
 
 def main():
